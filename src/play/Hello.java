84ff65dc4a082fd75c0891bbb6763988ef5dfae3
Ibou42 <i.souley@icloud.com>
2022-11-15 15:35:05 +0100

    dernier test

--- src/play/Hello.java
+++ src/play/Hello.java
@@ -3,11 +3,7 @@ package play;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		int a = 2*21;
-		System.out.println(a);
-
 	}
 
 }
