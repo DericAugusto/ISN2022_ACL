ef28fdfd87e9e7835c44a28aa7e2d237160776c5
Adrien <adrien.audren@gmail.com>
2022-11-23 19:53:44 +0100

    V1.1 player animation/ items animation/ items interaction

--- GameFiles/core/src/com/mygdx/game/Fighter.java
+++ GameFiles/core/src/com/mygdx/game/Fighter.java
@@ -1,16 +1,50 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
 public class Fighter {
 	
 	float xCoord;
 	float yCoord;
 	String pseudo;
+	int comp;
+	Texture Stand;
+	Texture[] WalkR;
+	Texture[] WalkL;
+	float textureHeight;
+	float textureWidth;
+	
+	float HP;
+	int currentSpeed;
+	float currentStrength;
 	
 	Fighter(String name){
 		
 		pseudo = name;
 		xCoord = 200;
 		yCoord = 100;
+		WalkR = new Texture[]{new Texture(Gdx.files.internal("WalkingPositionR1.png")),new Texture(Gdx.files.internal("WalkingPositionR2.png")),
+				new Texture(Gdx.files.internal("WalkingPositionR3.png")),new Texture(Gdx.files.internal("WalkingPositionR4.png")),
+				new Texture(Gdx.files.internal("WalkingPositionR5.png")),new Texture(Gdx.files.internal("WalkingPositionR6.png")),
+				new Texture(Gdx.files.internal("WalkingPositionR7.png")),new Texture(Gdx.files.internal("WalkingPositionR8.png")),
+				new Texture(Gdx.files.internal("WalkingPositionR9.png")),new Texture(Gdx.files.internal("WalkingPositionR10.png")),
+				new Texture(Gdx.files.internal("WalkingPositionR11.png")),new Texture(Gdx.files.internal("WalkingPositionR12.png")),};
+		Stand = new Texture(Gdx.files.internal("StandingPosition.png"));
+		WalkL = new Texture[]{new Texture(Gdx.files.internal("WalkingPositionL1.png")),new Texture(Gdx.files.internal("WalkingPositionL2.png")),
+				new Texture(Gdx.files.internal("WalkingPositionL3.png")),new Texture(Gdx.files.internal("WalkingPositionL4.png")),
+				new Texture(Gdx.files.internal("WalkingPositionL5.png")),new Texture(Gdx.files.internal("WalkingPositionL6.png")),
+				new Texture(Gdx.files.internal("WalkingPositionL7.png")),new Texture(Gdx.files.internal("WalkingPositionL8.png")),
+				new Texture(Gdx.files.internal("WalkingPositionL9.png")),new Texture(Gdx.files.internal("WalkingPositionL10.png")),
+				new Texture(Gdx.files.internal("WalkingPositionL11.png")),new Texture(Gdx.files.internal("WalkingPositionL12.png")),};
+		
+		textureHeight = Gdx.graphics.getHeight()/7;
+		textureWidth = Gdx.graphics.getWidth()/16;
+		
+		
+		HP = 1000; // initial HP
+		currentSpeed = 1; // initial speed
+		currentStrength = 1; // initial strength
 	}
 	
 	float[] locate() {
@@ -19,11 +53,40 @@ public class Fighter {
 		return location;
 		
 	}
-	void right() {
-		this.xCoord++;
+	Texture right() {
+		this.xCoord+=this.currentSpeed;
+		comp++;
+		return this.WalkR[((comp-1)/(4*this.currentSpeed))%12];
+		
+		// this.texture = 
+		// else if compt != 0 num frame compt+1%7 else num frame = 1
 	}
-	void left() {
-		this.xCoord--;
+	Texture left() {
+		this.xCoord-=this.currentSpeed;
+		comp++;
+		return this.WalkL[((comp-1)/(4*this.currentSpeed))%12];
+	}
+	
+	Texture stand() {
+		return this.Stand;
 	}
 
 }
+
+/*@startuml
+skinparam classAttributeIconSize 0
+class Dummy {
+	-field1
+	#field2
+	~method1()
+	+method2()
+}
+@enduml*/
+
+
+
+
+
+
+
+
---
+++ GameFiles/core/src/com/mygdx/game/Item.java
@@ -0,0 +1,99 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
+
+public class Item {
+	
+	float xCoord;
+	float yCoord;
+	enum itemType {healthPotion, strengthPotion, speedPotion}
+	itemType iT;
+	int comp;
+	Texture[] ItemSkins;
+	float textureHeight;
+	float textureWidth;
+	int deathCountdown;
+	int effectCountdown;
+	
+	Item(String itemName){
+		if(itemName == "healthPotion") {
+			this.iT = itemType.healthPotion;
+			this.ItemSkins = new Texture[]{new Texture(Gdx.files.internal("HealthPotion.png"))};
+		}
+		else if(itemName == "strengthPotion") {
+			this.iT = itemType.strengthPotion;
+			this.ItemSkins = new Texture[]{new Texture(Gdx.files.internal("StrengthPotion.png"))};
+		}
+		else if(itemName == "speedPotion") {
+			this.iT = itemType.speedPotion;
+			this.ItemSkins = new Texture[]{new Texture(Gdx.files.internal("SpeedPotion.png"))};
+		}
+		this.xCoord = MathUtils.random(Gdx.graphics.getWidth());
+		this.yCoord = Gdx.graphics.getHeight();
+		this.deathCountdown = 0;
+		
+		textureHeight = Gdx.graphics.getHeight()/30;
+		textureWidth = Gdx.graphics.getWidth()/120;
+	}
+	
+	float[] locate() {
+		
+		float[] location = new float[]{this.xCoord, this.yCoord};
+		return location;
+		
+	}
+	
+	void effect(Fighter player) {
+		
+		if (this.iT == itemType.healthPotion) {
+			player.HP += player.HP/2; 	// x 1,5
+			effectCountdown = 1;
+		}
+		
+		else if (this.iT == itemType.strengthPotion) {
+			player.currentStrength = player.currentStrength*2; // x2
+			effectCountdown = 1;
+		}
+		else if (this.iT == itemType.speedPotion) {
+			player.currentSpeed += player.currentSpeed*2; // x2
+			effectCountdown = 1;
+		}
+	}
+	
+	void clearEffect(Fighter player) {
+		
+		if (this.iT == itemType.healthPotion) {
+			player.HP -= player.HP/2; 	// x 1,5
+		}
+		
+		else if (this.iT == itemType.strengthPotion) {
+			player.currentStrength = player.currentStrength/2; // x2
+		}
+		else if (this.iT == itemType.speedPotion) {
+			player.currentSpeed = player.currentSpeed/2; // x2
+		}
+	}
+	
+	
+	Boolean move() {
+		
+		if (this.yCoord > 100) {
+			
+			this.yCoord -= 60*Gdx.graphics.getDeltaTime();
+			return false;
+		}
+		
+		else {
+			this.deathCountdown+=1;
+			
+			if(this.deathCountdown == 300) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+}
